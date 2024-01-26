e7866d978de0b411b1fe33641cbb347d84d0d6aa
livevil8 <jayamnithin27@gmail.com>
2024-01-26 20:26:01 +0000

    modules for terraform

---
+++ modules/Security-group/securityGrp.tf
@@ -0,0 +1,39 @@
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "http_server_sg" {
+    name = "django_docker_jenkins_sg"
+    vpc_id = aws_default_vpc.default.id
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 50000
+        to_port     = 50000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        name = "django_docker_jenkins_sg"
+    }
+
+}
